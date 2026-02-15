6cda60145c3b1d2e6865aed8e18b68f330d0316c
Las 3Ps <las3psdeldinero@gmail.com>
2026-02-15 10:59:51 +0100

    Add root landing page

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-2xl">Landing LAS3PS</h1>
+    </main>
+  );
+}
